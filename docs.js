88d88c710fc037d1c5ddd68ab334b6d336ede1bd
Sataca <glassg767@gmail.com>
2025-11-14 01:24:04 +0200

    commit

--- docs.js
+++ docs.js
@@ -1,14 +1,17 @@
 let meno = document.querySelector(".menu")
 let abrir = document.querySelector("#abre")
 let fechar = document.querySelector("#fecha")
+let conteudo = document.querySelector(".principal")
 
 function ope() {
     meno.style.display="block"
     abrir.style.display="none"
     fechar.style.display="block"
+    conteudo.style.display="none"
 }
 function clos() {
     meno.style.display="none"
     abrir.style.display="block"
     fechar.style.display="none"
+    conteudo.style.display=""
 }
